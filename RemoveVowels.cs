e6dc86620e1b922dabb85941a671e1f4914c2df4
Jamey Geoghagan <Jameygeoghagan@gmail.com>
2020-06-19 16:21:17 -0500

    Adding the file

---
+++ RemoveVowels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str="This website is for losers LOL!";
+            // lets figure out how to remove all the vowels
+            // a,e,i,o,u
+            var RemoveVowels = new string[] { "a", "e", "i", "o", "u" };
+            foreach (var c in RemoveVowels)
+            {
+                str = str.Replace(c, string.Empty);
+            }
+
+            Console.WriteLine(str);
+        }
+    }
+}
